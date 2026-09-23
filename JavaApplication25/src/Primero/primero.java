cb0934d6b2f60d96f7f5a404aab7d49bf1992259
bloodmaniac <juansefernando.guti2103@gmail.com>
2026-09-23 06:30:03 -0500

    firstExample

---
+++ JavaApplication25/src/Primero/primero.java
@@ -0,0 +1,17 @@
+package Primero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SebaGut2103
+ */
+public class primero {
+    public static void main(String[] args) {
+        Scanner nuevo = new Scanner(System.in);
+        System.out.println("Nombre?: ");
+        String nomb = nuevo.nextLine();
+        System.out.println("Hola " + nomb);
+    }
+    
+}
